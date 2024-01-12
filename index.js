b28d92d91a6d8864e7deba253933e62f34b5d3ea
Emmanuel <agueraemmanuel@gmail.com>
2024-01-12 17:50:41 -0300

    arreglos

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// const express = require('express')
-// // const productManager = require('./productsFile')
-
-// const app = express()
-// let addProduct = new productManager
-
-
-// app.get('/', (req, res) => {
-//     res.send('Bienvenidos a Sector Games Tandil')
-// })
-
-// app.get("allProducts", async (req, res) => {
-//     let response = await addProduct.productManager()
-//     res.send(response)
-// })
-
-
-// app.listen(8080, () => {
-//     console.log('Server run on port 8080')
-// })
-
 const express = require('express');
 const ProductManager = require('./productsFile');
 
