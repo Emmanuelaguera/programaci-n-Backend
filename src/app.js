78233e4d7f7040ad5e323679e1b4e65f38a41799
Emmanuel <agueraemmanuel@gmail.com>
2024-01-29 18:26:06 -0300

    arreglos en main home y realTimeProducts

--- src/app.js
+++ src/app.js
@@ -1,30 +1,68 @@
 const express = require("express");
 const handlebars = require('express-handlebars')
+const http = require('http')
+const { Server } = require('socket.io')
 
 const productsRoutes = require("./router/productsroutes");
 const cartRoutes = require("./router/cartsroutes");
 
+const ProductManager = require("./productsManager");
+const productManager = new ProductManager("./src/games.json");
+
 const app = express();
 const PORT = 8080 || process.env.PORT
 
+// SERVER HTTP  
+const server = http.createServer(app)
+
 //PUBLIC
-app.use(express.static(__dirname+"/public"))
+app.use(express.static(__dirname + "/public"))
 
 //ENGINE
 app.engine('handlebars', handlebars.engine())
 app.set('view engine', 'handlebars')
-app.set('views', __dirname+'/views')
+app.set('views', __dirname + '/views')
 
 app.use(express.json());
 //ROUTES
 app.use("/api", productsRoutes);
 app.use("/api", cartRoutes);
 
-app.get("/", (req, res) => {
-    res.render("home", {});
+app.get("/", async (req, res) => {
+    const products = await productManager.getProducts();
+    res.render("home", {
+        products
+    });
 });
 
-app.listen(PORT, () => {
+app.get("/realtimeproducts", async (req, res) => {
+    const products = await productManager.getProducts();
+    res.render("realTimeProducts", {
+        products
+    });
+});
+app.post("/", async (req, res) => {
+    const products = await productManager.getProducts();
+    res.render("home", {
+        products
+    });
+});
+
+
+//SOCKET SERVER
+const io = new Server(server)
+io.on('connection', (socket) => {
+    console.log('Hola nuevo cliente')
+    socket.on('new-product', async (newProduct) => {
+
+        console.log('Nuevo producto para agregar', newProduct);
+        await productManager.addProduct(newProduct);
+        socket.emit('update-products', await productManager.getProducts());
+
+    });
+})
+
+server.listen(PORT, () => {
     console.log("Server run on port 8080");
 });
 
